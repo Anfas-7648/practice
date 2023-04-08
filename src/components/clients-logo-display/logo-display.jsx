57258a2f96dfdcb21744bed8c9e3d5484180433e
Mohammed Anfas C <anfas7648@gmail.com>
2023-04-08 12:47:51 +0530

    Client logo display section added

--- src/components/clients-logo-display/logo-display.jsx
+++ src/components/clients-logo-display/logo-display.jsx
@@ -1,14 +1,30 @@
-// const logoDisplay = () => {
-//   <section className="logo-display-container">
-//     <div className="logo-display-section">
-//       <div className="logoContent"></div>
-//       <div className="logoImages">
-//         <div className="logoImageWrappe">
-//           <img />
-//         </div>
-//         r
-//       </div>
-//     </div>
-//   </section>;
-// };
-// export default logoDisplay;
+import "./logo-display.scss";
+import logo1 from "../../assets/companyLogos/apple-11 1.png";
+import logo2 from "../../assets/companyLogos/Group 13.png";
+import logo3 from "../../assets/companyLogos/slack-2 1.png";
+import logo4 from "../../assets/companyLogos/spotify-1 1.png";
+import logo5 from "../../assets/companyLogos/google-2015 1.png";
+const LogoDisplay = () => {
+  return (
+    <div className="logo-display-container">
+      <div className="logo-display-section">
+        <div className="logoContentSection">
+          <p className="logoContent">
+            Trusted by <br />
+            Global Companies
+          </p>
+        </div>
+        <div className="logoImages">
+          <div className="logoImageWrapper">
+            <img className="clientLogo" src={logo1} alt="clientLogo" />
+            <img className="clientLogo" src={logo2} alt="clientLogo" />
+            <img className="clientLogo" src={logo3} alt="clientLogo" />
+            <img className="clientLogo" src={logo4} alt="clientLogo" />
+            <img className="clientLogo" src={logo5} alt="clientLogo" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LogoDisplay;
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,11 +1,13 @@
 import "./home.scss";
 import Navigation from "../../components/navigation/navigation";
 import Herosection from "../../components/hero-section/hero-section";
+import LogoDisplay from "../../components/clients-logo-display/logo-display";
 const Home = () => {
   return (
     <div className="body-container">
       <Navigation />
       <Herosection />
+      <LogoDisplay />
     </div>
   );
 };
